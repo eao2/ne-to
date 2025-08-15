ce0c7fee91a1fe4ba67d87e96b40989f22aadde4
eao2 <96556227+eao2@users.noreply.github.com>
2025-08-15 21:36:27 +0800

    Update polyfill-dirname.js

--- server/plugins/polyfill-dirname.js
+++ server/plugins/polyfill-dirname.js
@@ -2,10 +2,10 @@
 import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirnameValue = dirname(__filename)
+export default () => {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirnameValue = dirname(__filename)
 
-globalThis.__filename = __filename
-globalThis.__dirname = __dirnameValue
-
-export default {} // ✅ Needed so Nuxt can import the plugin
+  globalThis.__filename = __filename
+  globalThis.__dirname = __dirnameValue
+}
